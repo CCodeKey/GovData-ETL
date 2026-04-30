1b1704f182fd3129cd432afff479bda41b524dba
CCodeKey <newcodiguin@gmail.com>
2026-04-29 21:19:29 -0300

    first commit

---
+++ src/main/java/gov/data/GovDataEtlProjectApplication.java
@@ -0,0 +1,13 @@
+package gov.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovDataEtlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GovDataEtlProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gov/data/GovDataEtlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovDataEtlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
